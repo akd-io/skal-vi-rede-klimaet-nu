4c872cd5cd0e03aa8c9faaf6ca8d41412cad68d1
Anders Kjær Damgaard <akd@akd.io>
2019-05-10 19:31:25 +0200

    Add gatsby-config.js

---
+++ src/gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Skal vi rede klimaet nu?`,
+    siteUrl: `https://skalviredeklimaet.nu`,
+    description: `En hjemmeside der besvarer spørgsmålet: Skal vi rede klimaet nu?`,
+  },
+};
