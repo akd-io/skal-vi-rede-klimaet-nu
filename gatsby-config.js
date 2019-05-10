ad92c40f5aa34fd11bbd9f73232950c74b4110be
Anders Kjær Damgaard <akd@akd.io>
2019-05-10 20:24:01 +0200

    Move gatsby-config and add Helmet

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Skal vi rede klimaet nu?',
+    siteUrl: 'https://skalviredeklimaet.nu',
+    description: 'En hjemmeside der besvarer spørgsmålet: Skal vi rede klimaet nu?',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+};
--- src/gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `Skal vi rede klimaet nu?`,
-    siteUrl: `https://skalviredeklimaet.nu`,
-    description: `En hjemmeside der besvarer spørgsmålet: Skal vi rede klimaet nu?`,
-  },
-};
