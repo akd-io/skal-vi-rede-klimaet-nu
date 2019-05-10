2d82d219f6331535e20f096c66c69d6fcf579926
Anders Kjær Damgaard <akd@akd.io>
2019-05-10 19:52:42 +0200

    Update index with new normalized style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import NormalizedLayout from '../components/NormalizedLayout';
 
 export default () => (
-  <>
-    <h1>Ja</h1>
-  </>
+  <NormalizedLayout>
+    <div
+      style={{
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1
+        style={{
+          display: 'inline-block',
+          margin: 0,
+          fontFamily:
+            '-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
+        }}
+      >
+        Ja
+      </h1>
+    </div>
+  </NormalizedLayout>
 );
